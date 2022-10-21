c39ebd7d8a53c9015d6f9583c742c5e145154e49
홍성민 <olnh22@gmail.com>
2022-10-21 20:59:17 +0900

    API 개발
    GET요청을 받고 모든 Student 요소를 return 해주는 API 개발

---
+++ Hong_DjangoSystem/student/api.py
@@ -0,0 +1,12 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import *
+
+class StudentList(APIView) :
+
+    def get(self, request):
+
+        model = Student.objects.all()
+        serializer = StudentSerializer(model, many=True)
+        return Response(serializer.data)
