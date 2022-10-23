d6bf7b15ca7f30b5530e3c92ec1531e9f65da661
홍성민 <olnh22@gmail.com>
2022-10-23 23:06:37 +0900

    API 개발
    student_id 별로 GET요청을 보내, 개별적인 모델 정보를 가져오는 API 개발

--- Hong_DjangoSystem/student/api.py
+++ Hong_DjangoSystem/student/api.py
@@ -3,17 +3,25 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
 
-class StudentList(APIView) :
+
+class StudentList(APIView):
 
     def get(self, request):
-
         model = Student.objects.all()
         serializer = StudentSerializer(model, many=True)
         return Response(serializer.data)
 
     def post(self, requset):
         serializer = StudentSerializer(data=requset.data)
-        if serializer.is_valid() :
+        if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class StudentDetail(APIView):
+
+    def get(self, request, student_id):
+        model = Student.objects.get(student_id=student_id)
+        serializer = StudentSerializer(model)
+        return Response(serializer.data)
