67a92702d3320d958688285bc7e6dc33c99cc42c
홍성민 <olnh22@gmail.com>
2023-04-28 02:30:39 +0900

    포트번호
    맥 이전 후 포트번호 수정

--- Direct Sale/app.py
+++ Direct Sale/app.py
@@ -130,4 +130,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all() # db생성
 
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
