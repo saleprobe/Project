9503744303a5cbeccc499cad66688cc4e0a01da7
홍성민 <olnh22@gmail.com>
2023-05-14 23:39:07 +0900

    json => DB 연동 작업
    json 형식 DB 연동 작업 중, mysql 데이터베이스에 led_status 테이블 생성된 상태(mysql 자체가 DB속의 DB의 이름)

--- SmartFarm/src/main/java/org/example/LEDController.java
+++ SmartFarm/src/main/java/org/example/LEDController.java
@@ -1,21 +1,37 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/led")
+@RequestMapping("/api")
+@Component
 public class LEDController {
 
-    // 서버에서의 LED 상태
-    private boolean isLedOn = false;
+    private final LedStatusRepository ledStatusRepository;
 
-    @GetMapping("/status")
-    public String getLEDStatus() {
-        return isLedOn ? "LED is on" : "LED is off";
+    @Autowired
+    public LEDController(LedStatusRepository ledStatusRepository) {
+        this.ledStatusRepository = ledStatusRepository;
     }
 
-    @PutMapping("/status")
-    public void setLEDStatus(@RequestParam("status") boolean status) {
-        isLedOn = status;
+    @PostMapping("/led")
+    public void setLEDStatus(@RequestBody String json) throws Exception {
+        json = "{\"ledStatus\": true}";  // 실험용 json
+
+        // JSON 파싱
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(json);
+        boolean ledStatus = root.path("ledStatus").asBoolean();
+
+        // DB에 저장
+        LEDStatus status = new LEDStatus(ledStatus);
+        ledStatusRepository.save(status);
+
+        System.out.println(json);
     }
+
 }
--- SmartFarm/src/main/java/org/example/Main.java
+++ SmartFarm/src/main/java/org/example/Main.java
@@ -1,12 +1,31 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+//@SpringBootApplication(scanBasePackages = {"org.example.LedStatusRepository"})
+//@SpringBootApplication(scanBasePackageClasses = Main.class)
+//@ComponentScan(basePackages = {"org.example.LEDController"})
+//@ComponentScan(basePackages = {"org.example.LedStatusRepository"})
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.example.LedStatusRepository"})
 public class Main {
 
+    @Autowired
+    private static LEDController ledController;
+
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication app = new SpringApplication(Main.class);
+        app.run(args);
+
+        String fakeJson = "{\"ledStatus\": true}";
+        try {
+            ledController.setLEDStatus(fakeJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
