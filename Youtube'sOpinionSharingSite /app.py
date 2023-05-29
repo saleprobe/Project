1d4327ca90c87c1ff3f5f92566e4b695843e7d96
홍성민 <olnh22@gmail.com>
2023-05-30 01:22:14 +0900

    게시물 구분
    게시물을 5개씩 보여주도록 페이지 분할하여 보여주도록 계속 구현 시도 중

--- Youtube'sOpinionSharingSite /app.py
+++ Youtube'sOpinionSharingSite /app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from jinja2 import Environment
 
 app = Flask(__name__)
 
@@ -8,6 +9,14 @@ posts = []
 # 페이지당 게시물 개수
 POSTS_PER_PAGE = 5
 
+# Jinja2 환경 설정
+env = Environment()
+
+def length(obj):
+    return len(obj)
+
+env.filters['length'] = length
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -16,17 +25,21 @@ def index():
         content = request.form.get('content')
         # 게시물 등록
         posts.append({'title': title, 'content': content})
-        return render_template('index.html', posts=posts)
-    # GET 요청이 들어오면 기존 게시물 목록을 템플릿에 전달
-    return render_template('index.html', posts=posts)
+
+    # GET 요청이 들어오면 페이지 번호를 1로 설정하여 기존 게시물 목록과 페이지 번호를 템플릿에 전달
+    return render_template('index.html', posts=posts, page=1, POSTS_PER_PAGE=POSTS_PER_PAGE)
 
 @app.route('/page/<int:page>', methods=['GET'])
 def view_page(page):
-    # 해당 페이지의 게시물 목록을 템플릿에 전달
-    return render_template('index.html', posts=get_posts_for_page(page), page=page)
+    # 해당 페이지의 게시물 목록과 페이지 번호를 템플릿에 전달
+    return render_template('index.html', posts=posts, page=page, POSTS_PER_PAGE=POSTS_PER_PAGE)
+
 
 def get_posts_for_page(page):
-    pass
+    # 페이지에 해당하는 게시물 목록을 반환
+    start_index = (page - 1) * POSTS_PER_PAGE
+    end_index = start_index + POSTS_PER_PAGE
+    return posts[::-1][start_index:end_index]
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
