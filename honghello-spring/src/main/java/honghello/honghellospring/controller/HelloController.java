59a0731e7ec494bcdac6cb1adb1b22ec36fc9370
홍성민 <olnh22@gmail.com>
2023-06-24 08:51:28 +0900

    HelloController
    웹 애플리케이션에서 Hello가 들어오면 hello 메소드를 호출 하는 컨트롤러 작성

---
+++ honghello-spring/src/main/java/honghello/honghellospring/controller/HelloController.java
@@ -0,0 +1,18 @@
+package honghello.honghellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+    
+}
