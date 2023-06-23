03a3db3f9bf757c81d4f8eae2a449d81a7d1da6e
홍성민 <olnh22@gmail.com>
2023-06-23 21:20:35 +0900

    스프링 부트 Application 실행
    스프링 부트 애플리케이션의 기본적인 설정과 실행을 담당하는 클래스 구현

---
+++ honghello-spring/src/main/java/honghello/honghellospring/HonghelloSpringApplication.java
@@ -0,0 +1,13 @@
+package honghello.honghellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonghelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonghelloSpringApplication.class, args);
+	}
+
+}
