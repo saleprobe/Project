543fc75b0c71348fe9da1f97fccea5b2fdbff2f2
홍성민 <olnh22@gmail.com>
2023-07-05 08:03:05 +0900

    회원 리포지토리 테스트 코드
    findAll에 관한 회원 리포지토리 테스트 코드 작성

--- honghello-spring/src/test/java/honghello/honghellospring/repository/MemoryMemberRepositoryTest.java
+++ honghello-spring/src/test/java/honghello/honghellospring/repository/MemoryMemberRepositoryTest.java
@@ -4,6 +4,8 @@ import honghello.honghellospring.domain.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
@@ -35,5 +37,20 @@ class MemoryMemberRepositoryTest {
         Member result = repository.findByName("spring1").get();
 
         assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
     }
 }
