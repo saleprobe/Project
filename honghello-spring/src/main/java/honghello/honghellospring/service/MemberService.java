334d23560b4d2bffa3f93edfbc0082b5983e3588
홍성민 <olnh22@gmail.com>
2023-07-08 09:25:24 +0900

    서비스 구현
    서비스 중 회원 관련 서비스 구현 (가령 회원 서비스)(회원가입)

---
+++ honghello-spring/src/main/java/honghello/honghellospring/service/MemberService.java
@@ -0,0 +1,29 @@
+package honghello.honghellospring.service;
+
+import honghello.honghellospring.domain.Member;
+import honghello.honghellospring.repository.MemberRepository;
+import honghello.honghellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+
+        validateDuplicateMember(member);  //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //같은 이름이 있는 중복 회원X
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+}
