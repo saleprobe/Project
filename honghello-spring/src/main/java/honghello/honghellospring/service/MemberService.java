6184e37995e677f54eac732b271dc9a06758d89d
홍성민 <olnh22@gmail.com>
2023-07-09 23:45:11 +0900

    서비스 구현
    전체 회원 조회 및 한 명만 조회 구현

--- honghello-spring/src/main/java/honghello/honghellospring/service/MemberService.java
+++ honghello-spring/src/main/java/honghello/honghellospring/service/MemberService.java
@@ -4,6 +4,7 @@ import honghello.honghellospring.domain.Member;
 import honghello.honghellospring.repository.MemberRepository;
 import honghello.honghellospring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -25,5 +26,16 @@ public class MemberService {
         result.ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(long memberId){
+        return memberRepository.findById(memberId);
     }
 }
