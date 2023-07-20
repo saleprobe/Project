a2040efb03cfcecfc202d9a44f2926452c948c77
홍성민 <olnh22@gmail.com>
2023-07-20 21:52:38 +0900

    JPA를 위한 설정 변경
    1. 서비스 계층에 트랜잭션 추가
    2. JPA를 사용하도록 스프링 설정 변경

--- honghello-spring/src/main/java/honghello/honghellospring/SpringConfig.java
+++ honghello-spring/src/main/java/honghello/honghellospring/SpringConfig.java
@@ -1,13 +1,26 @@
 package honghello.honghellospring;
 
+import honghello.honghellospring.repository.JpaMemberRepository;
 import honghello.honghellospring.repository.MemberRepository;
 import honghello.honghellospring.repository.MemoryMemberRepository;
 import honghello.honghellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
     }
 }
