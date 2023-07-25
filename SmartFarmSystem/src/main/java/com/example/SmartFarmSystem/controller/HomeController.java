ae6dd06d95c6fbba4a512372c167a9a492b927fb
홍성민 <olnh22@gmail.com>
2023-07-25 09:07:18 +0900

    HomeController
    메인페이지로 이동하는 컨트롤러 구현

---
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.SmartFarmSystem.controller;
+
+import com.example.SmartFarmSystem.service.SmartFarmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    private final SmartFarmService smartFarmService;
+
+    @Autowired
+    public HomeController(SmartFarmService smartFarmService) {
+        this.smartFarmService = smartFarmService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "uploadpage";
+    }
+
+}
