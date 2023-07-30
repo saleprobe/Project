c50393a53436ab3b3ca9bd2879362786b9d0bf78
홍성민 <olnh22@gmail.com>
2023-07-30 23:52:59 +0900

    Create SmartFarmRepository.java
    리포지토리 영역의 SmartFarmRepository를 스프링 데이터 JPA이용하여 구현

---
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/repository/SmartFarmRepository.java
@@ -0,0 +1,10 @@
+package com.example.SmartFarmSystem.repository;
+
+import com.example.SmartFarmSystem.domain.SmartFarm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SmartFarmRepository extends JpaRepository<SmartFarm, Long> {
+
+}
