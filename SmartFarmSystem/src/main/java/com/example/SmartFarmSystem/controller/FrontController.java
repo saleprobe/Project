9d3bae1940a9ab639ca21061adee3d264d4977e9
홍성민 <olnh22@gmail.com>
2023-08-04 09:28:29 +0900

    스프링 빈 오류
    스프링 빈에 등록되지 않았던 오류 해결

--- SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/FrontController.java
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/FrontController.java
@@ -3,10 +3,12 @@ package com.example.SmartFarmSystem.controller;
 import com.example.SmartFarmSystem.domain.SmartFarm;
 import com.example.SmartFarmSystem.service.SmartFarmService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class FrontController {
     @Autowired
     private final SmartFarmService smartFarmService;
