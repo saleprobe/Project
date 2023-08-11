f80a3d7a923ec4dbfa1514abe299dae5556e1235
홍성민 <olnh22@gmail.com>
2023-08-11 20:23:49 +0900

    Update HardWareController.java
    Get매핑은 DB내의 데이터 Json형식으로 주고, Post매핑은 외부에서 처리된 Json값을 저장하도록 코드 구현 및 수정

--- SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/HardWareController.java
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/HardWareController.java
@@ -4,6 +4,7 @@ import com.example.SmartFarmSystem.domain.SmartFarm;
 import com.example.SmartFarmSystem.service.SmartFarmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,12 +19,19 @@ public class HardWareController {
         this.smartFarmService = smartFarmService;
     }
 
+    @GetMapping("/raspberrypi")
+    @ResponseBody
+    public SmartFarm getdata_r(){
+        System.out.println("Get Request checked(/raspberrypi)");
+        return smartFarmService.getLastSmartFarm();
+    }
+
     @PostMapping("/raspberrypi")
     @ResponseBody
-    public SmartFarm raspberrypi(@RequestBody SmartFarm smartFarm) throws Exception {
-        System.out.println("Received JSON data: " + smartFarm);
-        smartFarmService.saveMemo(smartFarm);
+    public void savedata_r(@RequestBody SmartFarm smartFarm) throws Exception {
+        System.out.println("Received JSON data: " + smartFarm + "from raspberrypi");
 
-        return smartFarmService.getLastSmartFarm();
+        // 데이터베이스에 Json 데이터를 처리하고 저장
+        smartFarmService.saveSF(smartFarm);
     }
 }
