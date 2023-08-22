fae165d0496334d5aeef749da8c1492b955e5824
홍성민 <olnh22@gmail.com>
2023-08-22 23:25:57 +0900

    Create RaspberryPiWebSocketHandler.java
    최근 튜플 값을 socket으로 전달하는 socket handler 작성

---
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/RaspberryPiWebSocketHandler.java
@@ -0,0 +1,30 @@
+package com.example.SmartFarmSystem;
+
+import com.example.SmartFarmSystem.domain.SmartFarm;
+import com.example.SmartFarmSystem.service.SmartFarmService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class RaspberryPiWebSocketHandler extends TextWebSocketHandler {
+
+    private final SmartFarmService smartFarmService;
+
+    @Autowired
+    public RaspberryPiWebSocketHandler(SmartFarmService smartFarmService) {
+        this.smartFarmService = smartFarmService;
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // MySQL 데이터베이스에서 최근 튜플을 조회하고 JSON으로 변환하여 클라이언트에게 전송
+        SmartFarm latestSmartFarm = smartFarmService.getLastSmartFarm(); // 최근 튜플 조회하는 메소드 예시
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(latestSmartFarm);
+        session.sendMessage(new TextMessage(json));
+    }
+}
