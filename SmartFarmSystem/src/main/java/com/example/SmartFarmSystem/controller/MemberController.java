dc5a83037eb09d458842d0618adafd3d8174b897
홍성민 <olnh22@gmail.com>
2023-08-31 23:25:31 +0900

    Create MemberController.java
    멤버서비스를 이용해 회원가입을 수행하는 registerMember 메소드 작성, 멤버서비스를 이용해 로그인을 수행하는 loginMember 메소드를 작성한 멤버 컨트롤러 구현

---
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.example.SmartFarmSystem.controller;
+
+import com.example.SmartFarmSystem.domain.Member;
+import com.example.SmartFarmSystem.domain.SmartFarm;
+import com.example.SmartFarmSystem.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MemberController {
+
+    private MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/member/register")
+    @ResponseBody
+    public void registerMember(@RequestBody Member member) throws Exception {
+        System.out.println("member registered!");
+        memberService.registerMember(member);
+    }
+
+    @PostMapping("/member/login")
+    @ResponseBody
+    public ResponseEntity<String> loginMember(@RequestBody Member member) {
+        boolean isLoggedIn = memberService.loginMember(member.getmid(), member.getmpw());
+//        boolean isLoggedIn = true;
+        if (isLoggedIn) {
+            System.out.println("INFO  Member Login Success");
+            return new ResponseEntity<>("Login Success", HttpStatus.OK);
+        } else {
+            System.out.println("INFO  Member Login Failure");
+            return new ResponseEntity<>("Login Failure", HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
