d897d68e9530759f989069839c29c6a43b9ac62c
홍성민 <olnh22@gmail.com>
2023-09-01 20:19:33 +0900

    중복 회원 검증
    중복된 memberId를 가지고 있으면 회원가입이 불가능하게 막음

--- SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/service/MemberService.java
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/service/MemberService.java
@@ -5,6 +5,8 @@ import com.example.SmartFarmSystem.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class MemberService {
 
@@ -16,11 +18,20 @@ public class MemberService {
     }
 
     public void registerMember(Member member) {
+        validateDuplicateMember(member);  //중복 회원 검증
         memberRepository.save(member);
     }
 
     public boolean loginMember(String memberId, String memberPw) {
         Member member = memberRepository.findByMemberIdAndMemberPw(memberId, memberPw);
         return member != null; // 멤버가 존재하면 true, 존재하지 않으면 false를 반환
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //같은 이름이 있는 중복 회원X
+        Optional<Member> result = memberRepository.findByMemberId(member.getmid());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
     }
 }
