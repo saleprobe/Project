d198db6affd11ab9a933e80b6a39ea6bd69a8ecf
홍성민 <olnh22@gmail.com>
2023-09-07 20:51:30 +0900

    리포지토리 변경
    로그인 구조 개선으로 인한 리포지토리 변경

---
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.SmartFarmSystem.repository;
+
+import com.example.SmartFarmSystem.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByLoginId(String loginId);
+    boolean existsByNickname(String nickname);
+    Optional<User> findByLoginId(String loginId);
+}
