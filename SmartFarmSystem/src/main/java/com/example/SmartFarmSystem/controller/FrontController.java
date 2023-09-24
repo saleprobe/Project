4e1d7c9129575a0e9c049cfda47c948135aad89c
홍성민 <olnh22@gmail.com>
2023-09-24 20:57:11 +0900

    재등록
    프로젝트 파일 재생성 후 재등록

--- SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/FrontController.java
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/FrontController.java
@@ -1,32 +1,32 @@
-package com.example.SmartFarmSystem.controller;
-
-import com.example.SmartFarmSystem.domain.SmartFarm;
-import com.example.SmartFarmSystem.service.SmartFarmService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.time.LocalDateTime;
-
-@Controller
-public class FrontController {
-    @Autowired
-    private final SmartFarmService smartFarmService;
-
-    public FrontController(SmartFarmService smartFarmService) {
-        this.smartFarmService = smartFarmService;
-    }
-
-    @PostMapping("/react")
-    @ResponseBody
-    public SmartFarm react(@RequestBody SmartFarm smartFarm) throws Exception {
-        LocalDateTime currentTime = LocalDateTime.now();
-        System.out.println("INFO  post request checked(/react) " + currentTime);
-        smartFarmService.join(smartFarm);
-
-        return smartFarmService.getLastSmartFarm();
-    }
-}
+//package com.example.SmartFarmSystem.controller;
+//
+//import com.example.SmartFarmSystem.domain.SmartFarm;
+//import com.example.SmartFarmSystem.service.SmartFarmService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.time.LocalDateTime;
+//
+//@Controller
+//public class FrontController {
+//    @Autowired
+//    private final SmartFarmService smartFarmService;
+//
+//    public FrontController(SmartFarmService smartFarmService) {
+//        this.smartFarmService = smartFarmService;
+//    }
+//
+//    @PostMapping("/react")
+//    @ResponseBody
+//    public SmartFarm react(@RequestBody SmartFarm smartFarm) throws Exception {
+//        LocalDateTime currentTime = LocalDateTime.now();
+//        System.out.println("INFO  post request checked(/react) " + currentTime);
+//        smartFarmService.join(smartFarm);
+//
+//        return smartFarmService.getLastSmartFarm();
+//    }
+//}
--- SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/HardWareController.java
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/controller/HardWareController.java
@@ -1,41 +1,41 @@
-package com.example.SmartFarmSystem.controller;
-
-import com.example.SmartFarmSystem.domain.SmartFarm;
-import com.example.SmartFarmSystem.service.SmartFarmService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.time.LocalDateTime;
-
-@Controller
-public class HardWareController {
-
-    @Autowired
-    private final SmartFarmService smartFarmService;
-
-    public HardWareController(SmartFarmService smartFarmService) {
-        this.smartFarmService = smartFarmService;
-    }
-
-    @GetMapping("/raspberrypi")
-    @ResponseBody
-    public SmartFarm getdata_r(){
-        LocalDateTime currentTime = LocalDateTime.now();
-        System.out.println("Get Request checked(/raspberrypi) " + currentTime);
-        return smartFarmService.getLastSmartFarm();
-    }
-
-    @PostMapping("/raspberrypi")
-    @ResponseBody
-    public void savedata_r(@RequestBody SmartFarm smartFarm) throws Exception {
-        LocalDateTime currentTime = LocalDateTime.now();
-        System.out.println("Received JSON data: " + smartFarm + " from raspberrypi " + currentTime);
-
-        // 데이터베이스에 Json 데이터를 처리하고 저장
-        smartFarmService.join(smartFarm);
-    }
-}
+//package com.example.SmartFarmSystem.controller;
+//
+//import com.example.SmartFarmSystem.domain.SmartFarm;
+//import com.example.SmartFarmSystem.service.SmartFarmService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.time.LocalDateTime;
+//
+//@Controller
+//public class HardWareController {
+//
+//    @Autowired
+//    private final SmartFarmService smartFarmService;
+//
+//    public HardWareController(SmartFarmService smartFarmService) {
+//        this.smartFarmService = smartFarmService;
+//    }
+//
+//    @GetMapping("/raspberrypi")
+//    @ResponseBody
+//    public SmartFarm getdata_r(){
+//        LocalDateTime currentTime = LocalDateTime.now();
+//        System.out.println("Get Request checked(/raspberrypi) " + currentTime);
+//        return smartFarmService.getLastSmartFarm();
+//    }
+//
+//    @PostMapping("/raspberrypi")
+//    @ResponseBody
+//    public void savedata_r(@RequestBody SmartFarm smartFarm) throws Exception {
+//        LocalDateTime currentTime = LocalDateTime.now();
+//        System.out.println("Received JSON data: " + smartFarm + " from raspberrypi " + currentTime);
+//
+//        // 데이터베이스에 Json 데이터를 처리하고 저장
+//        smartFarmService.join(smartFarm);
+//    }
+//}
