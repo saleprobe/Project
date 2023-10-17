48d09461cccd45ac1e9d07669001477f2f1f5fa3
홍성민 <olnh22@gmail.com>
2023-10-17 23:05:49 +0900

    비활성화
    쓰지않는 코드 비활성화

--- SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/WebSocketConfig.java
+++ SmartFarmSystem/src/main/java/com/example/SmartFarmSystem/WebSocketConfig.java
@@ -1,37 +1,37 @@
-package com.example.SmartFarmSystem;
-
-import com.example.SmartFarmSystem.handler.RaspberryPiWebSocketHandler;
-import com.example.SmartFarmSystem.handler.ReactWebSocketHandler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-
-@Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-
-    @Autowired
-    private final RaspberryPiWebSocketHandler raspberryPiWebSocketHandler;
-
-    @Autowired
-    private final ReactWebSocketHandler reactWebSocketHandler;
-
-    public WebSocketConfig(RaspberryPiWebSocketHandler raspberryPiWebSocketHandler, ReactWebSocketHandler reactWebSocketHandler) {
-        this.raspberryPiWebSocketHandler = raspberryPiWebSocketHandler;
-        this.reactWebSocketHandler = reactWebSocketHandler;
-    }
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(raspberryPiWebSocketHandler, "/raspberrypi_websocket").setAllowedOrigins("*");
-        registry.addHandler(reactWebSocketHandler, "/react_websocket").setAllowedOrigins("*");
-        System.out.println("INFO  Spring Boot Embedded Socket Request checked");
-    }
-
-}
-
-
-
+//package com.example.SmartFarmSystem;
+//
+//import com.example.SmartFarmSystem.handler.RaspberryPiWebSocketHandler;
+//import com.example.SmartFarmSystem.handler.ReactWebSocketHandler;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.socket.config.annotation.EnableWebSocket;
+//import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+//import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+//
+//
+//@Configuration
+//@EnableWebSocket
+//public class WebSocketConfig implements WebSocketConfigurer {
+//
+//    @Autowired
+//    private final RaspberryPiWebSocketHandler raspberryPiWebSocketHandler;
+//
+//    @Autowired
+//    private final ReactWebSocketHandler reactWebSocketHandler;
+//
+//    public WebSocketConfig(RaspberryPiWebSocketHandler raspberryPiWebSocketHandler, ReactWebSocketHandler reactWebSocketHandler) {
+//        this.raspberryPiWebSocketHandler = raspberryPiWebSocketHandler;
+//        this.reactWebSocketHandler = reactWebSocketHandler;
+//    }
+//
+//    @Override
+//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+//        registry.addHandler(raspberryPiWebSocketHandler, "/raspberrypi_websocket").setAllowedOrigins("*");
+//        registry.addHandler(reactWebSocketHandler, "/react_websocket").setAllowedOrigins("*");
+//        System.out.println("INFO  Spring Boot Embedded Socket Request checked");
+//    }
+//
+//}
+//
+//
+//
