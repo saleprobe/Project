f4d54f4fb290061e26078b323b23829b5d00973e
홍성민 <olnh22@gmail.com>
2023-10-21 23:41:55 +0900

    API 개발
    GET요청을 받고 모든 Student 요소를 return 해주는 API 개발

--- Hong_DjangoSystem/Hong_DjangoSystem/urls.py
+++ Hong_DjangoSystem/Hong_DjangoSystem/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from student.api import StudentList
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/student_list", StudentList.as_view(), name="student_list"),
+
 ]
